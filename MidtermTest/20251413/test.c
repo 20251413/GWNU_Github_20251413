b026f27ac41e7683d7d0618453dc655039898a34
20251413 <potato1sgood@gwnu.ac.kr>
2025-04-15 14:47:33 +0900

    2nd save

--- MidtermTest/20251413/test.c
+++ MidtermTest/20251413/test.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <conio.h>
+#include <Windows.h>
 
 void gotoxy(int x, int y)
-}
-    COORD pos = { x,y };
+{
+    COORD pos;
+    pos.X = x;
+    pos.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+int main()
+{
+    printf("                                            (\033[47mT\033[47mT\033[47mT\033[47mT\033[0m)                    T\n");
+    printf("                                           (\033[47mT\033[47mT\033[47mT\033[0m)                      T\n");
+    printf("                                          (\033[47mT\033[47mT\033[47mT\033[0m)                       T\n");
+    printf("                                          (\033[47mT\033[47mT\033[0m(                        T\n");
+    printf("                 _________________________|  |__                      T\n");
+    printf("                /\\                              \\                     T\n");
+    printf("               //_\\                              \\                    T\n");
+    printf("TEST\n");
+    printf("TEST\n");
+    printf("TEST\n");
+    printf("TEST\n");
+    printf("TEST\n");
+    printf("TEST\n");
+    printf("TEST\n");
+    printf("TEST\n");
+    printf("TEST\n");
+
+    return 0;
 }

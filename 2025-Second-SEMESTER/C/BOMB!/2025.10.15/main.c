51bdee8c13eae00fa0db497aa1f17b39564bd272
20251413 <potato1sgood@gwnu.ac.kr>
2025-10-15 14:42:18 +0900

    C 동적할당과 이중배열

---
+++ 2025-Second-SEMESTER/C/BOMB!/2025.10.15/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int chess[10][10];
+
+	int i = 0;
+	int j = 0;
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			chess[i][j] = (i + j) % 2;
+			printf("\t%d", chess[i][j]);
+		}
+		printf("\n");
+	}
+}
